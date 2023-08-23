ef66126314ba4608c40028d0883f008f96635f97
Chosh93 <chosh930911@gmail.com>
2023-08-23 16:21:00 +0900

    230823 / 카카오맵 원표시

--- recipe_restaurant_fe/src/pages/RestaurantPage.js
+++ recipe_restaurant_fe/src/pages/RestaurantPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { styled } from "styled-components";
 
 const Container = styled.div`
@@ -15,12 +15,14 @@ const MapContainer = styled.div`
 
 const RestaurantPage = () => {
     const [map, setMap] = useState(null);
+    const [marker, setMarker] = useState(null);
+    const [circle, setCircle] = useState(null);
 
     useEffect(() => {
         const container = document.getElementById('map');
         const options = {
             center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-            level: 3,
+            level: 2,
         };
 
         const createdMap = new window.kakao.maps.Map(container, options);
@@ -34,16 +36,46 @@ const RestaurantPage = () => {
                 const message = '<div style="padding:5px;">여기에 계신가요?!</div>';
 
                 displayMarker(createdMap, locPosition, message);
+                displayCircle(createdMap, locPosition);
             });
         } else {
             const locPosition = new window.kakao.maps.LatLng(33.450701, 126.570667);
             const message = 'geolocation을 사용할 수 없어요..';
             displayMarker(createdMap, locPosition, message);
+            displayCircle(createdMap, locPosition);
         }
-        
-        const circle = new window.kakao.maps.Circle({
-            center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-            radius: 50,
+    }, []);
+
+    const displayMarker = (map, locPosition, message) => {
+        if (!map) return;
+
+        const newMarker = new window.kakao.maps.Marker({
+            map: map,
+            position: locPosition,
+            draggable: true,
+        });
+
+        const infowindow = new window.kakao.maps.InfoWindow({
+            content: message,
+            removable: true,
+        });
+
+        infowindow.open(map, newMarker);
+        map.setCenter(locPosition);
+        setMarker(newMarker);
+
+        // 마커의 드래그 이벤트 핸들러
+        newMarker.addListener('dragend', function() {
+            const newPosition = newMarker.getPosition();
+            map.panTo(newPosition);
+            updateCirclePosition(newPosition);
+        });
+    };
+
+    const displayCircle = (map, centerPosition) => {
+        const newCircle = new window.kakao.maps.Circle({
+            center: centerPosition,
+            radius: 1000,
             strokeWeight: 5,
             strokeColor: '#75B8FA',
             strokeOpacity: 1,
@@ -52,32 +84,20 @@ const RestaurantPage = () => {
             fillOpacity: 0.7,
         });
 
-        circle.setMap(createdMap);
-    }, []);
+        newCircle.setMap(map);
+        setCircle(newCircle);
+    };
 
-    const displayMarker = (map, locPosition, message) => {
-        if (!map) return;
-
-        const marker = new window.kakao.maps.Marker({
-            map: map,
-            position: locPosition,
-        });
-
-        const iwContent = message;
-        const iwRemoveable = true;
-
-        const infowindow = new window.kakao.maps.InfoWindow({
-            content: iwContent,
-            removable: iwRemoveable,
-        });
-
-        infowindow.open(map, marker);
-        map.setCenter(locPosition);
+    const updateCirclePosition = (newPosition) => {
+        if (circle) {
+            circle.setMap(null); // 기존의 원 삭제
+            displayCircle(map, newPosition); // 새 원 추가
+        }
     };
 
     return (
         <Container>
-        <MapContainer id="map" ></MapContainer>
+            <MapContainer id="map" ></MapContainer>
         </Container>
     )
 }
