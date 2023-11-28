fec9a1daebd8ced7cd77a27546295b2c85eaf171
Chosh93 <chosh930911@gmail.com>
2023-11-28 21:02:46 +0900

    랭크컴포넌트 슬라이스 추가

--- recipe_restaurant_fe/src/components/Recipe/RecipeRank.js
+++ recipe_restaurant_fe/src/components/Recipe/RecipeRank.js
@@ -4,21 +4,26 @@ import recipeApi from "../../api/recipeApi";
 import { Link } from "react-router-dom";
 
 const Container = styled.div`
-    display: grid;
-    margin-top: 10px;
-    align-content: center;
-    justify-content: center;
-    grid-template-columns: repeat(auto-fit, 300px);
-    gap: 1rem;
+  width: 100%;
+  margin-top: 10px;
+  overflow: hidden;
 `;
 
-const CardView = styled.div`
-    background-color: white;
-    box-shadow: 1px 1px 5px black;
-    display: flex;
-    flex-direction: column;
-    height: 320px;
-    padding: 15px 24px;
+const Slider = styled.ul`
+  display: flex;
+  list-style: none;
+  transition: transform 0.3s ease-in-out;
+`;
+
+const SlideItem = styled.li`
+  background-color: white;
+  box-shadow: 1px 1px 5px black;
+  display: flex;
+  flex-direction: column;
+  height: 320px;
+  margin: 0 12px 40px 0;
+  padding: 15px 24px;
+  max-width: 300px;
 `;
 
 const CardItemHeader = styled.div`
@@ -43,16 +48,17 @@ const StyledLink = styled(Link)`
   }
 `;
 
-const RecipeRank = () => {
+const RecipeRankSlider = () => {
   const [rankList, setRankList] = useState([]);
   const [recipeInfoList, setRecipeInfoList] = useState([]);
-  
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await recipeApi.getRecipeRank();
         setRankList(response.data.slice(0, 10));
-  
+
         const promises = response.data.map((recipe) =>
           recipeApi.getRecipeInfo(recipe.foodId)
         );
@@ -62,24 +68,39 @@ const RecipeRank = () => {
         console.log(error);
       }
     };
-  
+
     fetchData();
   }, []);
 
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % rankList.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + rankList.length) % rankList.length);
+  };
+
   return (
-    <div>
+    <>
+    <div>조회수 1위부터 10위까지의 레시피 입니다.</div>
       <Container>
-        {rankList.map((recipe, index) => (
-          <StyledLink key={index} to={`/recipe/${recipe.foodId}`}>
-            <CardView>
-              <CardItemImage src={recipeInfoList[index]?.thumbImg} alt={recipeInfoList[index]?.name} />
-              <CardItemHeader>{recipeInfoList[index]?.name}</CardItemHeader>
-            </CardView>
-          </StyledLink>
-        ))}
+        <Slider style={{ transform: `translateX(-${currentIndex * 300}px)` }}>
+          {rankList.map((recipe, index) => (
+            <StyledLink key={index} to={`/recipe/${recipe.foodId}`}>
+              <SlideItem>
+                <CardItemImage src={recipeInfoList[index]?.thumbImg} alt={recipeInfoList[index]?.name} />
+                <CardItemHeader>{recipeInfoList[index]?.name}</CardItemHeader>
+              </SlideItem>
+            </StyledLink>
+          ))}
+        </Slider>
       </Container>
-    </div>
+      <div>
+        <button onClick={prevSlide}>이전</button>
+        <button onClick={nextSlide}>다음</button>
+      </div>
+    </>
   );
 };
 
-export default RecipeRank;
+export default RecipeRankSlider;
